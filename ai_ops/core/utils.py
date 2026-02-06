576542c92b5ecdf25e83d2b9b3e5f9c57e324ee6
Onkar Shahapurkar <163027200+Onkar-Shahapurkar@users.noreply.github.com>
2026-02-06 13:53:37 +0530

    Delete ai_ops directory

--- ai_ops/core/utils.py
+++
@@ -1,9 +0,0 @@
-import logging
-from tenacity import retry, stop_after_attempt, wait_exponential
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("ai_ops_assistant")
-
-@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=2, max=10))
-def safe_api_call(func, *args, **kwargs):
-    return func(*args, **kwargs)
